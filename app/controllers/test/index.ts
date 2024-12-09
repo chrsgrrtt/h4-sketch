fa0e9dad366d8fdb99a57fb02e7a6578f7f87dbe
Chris Garrett <chris@c8va.com>
2024-12-09 15:59:21 +0000

    .

--- app/controllers/test/index.ts
+++ app/controllers/test/index.ts
@@ -1,7 +1,7 @@
 import { H4BaseController } from "../../../h4/server/controller";
 import { testRepository } from "../../models/test";
 
-export default class SingleTestController extends H4BaseController {
+export default class TestsController extends H4BaseController {
 	get = async () => {
 		const testRecords = await testRepository.query().limit(10).all();
 
